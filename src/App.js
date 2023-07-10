50a2ceff2aeacc904e788f48933daa98ed6f3145
boopathyganesh <dhivyavn.teammortals.official@gmail.com>
2023-07-10 20:24:20 +0530

    responsive changes

--- src/App.js
+++ src/App.js
@@ -59,116 +59,114 @@ function Portfolio() {
                 </div>
               </div>
               <Container fluid className='main'>
-              <div className='auth-card'>
-                <div className='details'>
-                  <div className='auth-details'>
-                    <div className='auth-name'>
-                      <h1>Nicolas Simons -</h1>
-                    </div>
-                    <div className='role'>
-                      <div className='inner-area'>
-                        <div className='general'>
-                          <p>Support Manager</p>
-                          <div className='arrow-icon'>
-                            <ArrowForward className='props'/>
-                            <ArrowForward className='props'/>
+                <div className='auth-card'>
+                    <div className='auth-details'>
+                      <div className='auth-name'>
+                        <h1>Nicolas Simons -</h1>
+                      </div>
+                      <div className='role'>
+                        <div className='inner-area'>
+                          <div className='general'>
+                            <p>Support Manager</p>
+                            <div className='arrow-icon'>
+                              <ArrowForward className='props'/>
+                              <ArrowForward className='props'/>
+                            </div>
                           </div>
-                        </div>
-                        <div className='specific'>
-                          <p>Art Director - MetroLab</p>
+                          <div className='specific'>
+                            <p>Art Director - MetroLab</p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className='auth-profile'>
-                    <div className='profile-img image'>
+                    <div className='auth-profile'>
+                      <div className='profile-img image'>
+                      </div>
+                      <div className='donation-btn'>
+                        <p>Send Donation</p>
+                      </div>
                     </div>
-                    <div className='donation-btn'>
-                      <p>Send Donation</p>
-                    </div>
+                  </div>
+                <div className='description-section'>
+                  <div className='tabs'>
+                    <div className='tab'><p>ART DIRECTOR</p></div>
+                    <div className='tab'><p>12 DECEMBER 2022</p></div>
+                    <div className='tab'><p>PORTFOLIO</p></div>
+                  </div>
+                  <div className='description'>
+                    <p>MetroLab / 2023© creates interesting website dashboard design for company artists and photographers.</p>
                   </div>
                 </div>
-              </div>
-              <div className='description-section'>
-                <div className='tabs'>
-                  <div className='tab'><p>ART DIRECTOR</p></div>
-                  <div className='tab'><p>12 DECEMBER 2022</p></div>
-                  <div className='tab'><p>PORTFOLIO</p></div>
-                </div>
-                <div className='description'>
-                  <p>MetroLab / 2023© creates interesting website dashboard design for company artists and photographers.</p>
-                </div>
-              </div>
               </Container>
               <Container fluid className='posts'>
-              <div className='latest-post'>
-                <div className='header'>
-                  <div className='tab'>
-                    <p>LATEST POST</p>
-                  </div>
-                  <div className='arrow-icon'>
-                    <ArrowForward className='props'/>
-                    <ArrowForward className='props'/>
-                  </div>
-                </div>
-                <div className='post-img'>
-                  <div className='img-holder image'></div>
-                </div>
-                <div className='title'>
-                  <p>Design Generate</p>
-                  <div className='views'>
-                    <p>45k views</p>
-                  </div>
-                </div>
-                <div className='h-line'></div>
-                <div className='description'>
-                  <p>
-                    Testing Popular <strong><u>designing tools</u></strong> and evaluating them with MetroLab crew and artists.
-                  </p>
-                </div>
-              </div>
-              <div className='favorite-post'>
-                <div className='header'>
-                  <div className='tab'>
-                    <p>FAVORITE POST</p>
-                  </div>
-                  <div className='arrow-icon'>
-                    <ArrowForward className='props'/>
-                    <ArrowForward className='props'/>
-                  </div>
-                </div>
-                <div className='data'>
-                  <p>Training new Participants</p>
-                  <div className='value'>
-                    <p>38.2</p>
-                  </div>
-                </div>
-              </div>
-              <div className='statistics'>
-                <div className='header'>
-                  <div className='tab'>
-                    <p>STATISTICS</p>
-                  </div>
-                  <div className='arrow-icon'>
-                    <ArrowForward className='props'/>
-                    <ArrowForward className='props'/>
-                  </div>
-                </div>
-                <div className='data'>
-                  <p>Training new Participants</p>
-                  <div className='graph'>
-                    <div className='vbars'>
-                      <div className='bar val1'></div>
-                      <div className='bar val2'></div>
-                      <div className='bar val3'></div>
-                      <div className='bar val4'></div>
-                      <div className='bar val5'></div>
-                      <div className='bar val6'></div>
+                <div className='latest-post'>
+                  <div className='header'>
+                    <div className='tab'>
+                      <p>LATEST POST</p>
                     </div>
-                    <div className='h-line'></div>
+                    <div className='arrow-icon'>
+                      <ArrowForward className='props'/>
+                      <ArrowForward className='props'/>
+                  </div>
+                  </div>
+                  <div className='post-img'>
+                    <div className='img-holder image'></div>
+                  </div>
+                  <div className='title'>
+                    <p>Design Generate</p>
+                    <div className='views'>
+                      <p>45k views</p>
+                    </div>
+                  </div>
+                  <div className='h-line'></div>
+                  <div className='description'>
+                    <p>
+                      Testing Popular <strong><u>designing tools</u></strong> and evaluating them with MetroLab crew and artists.
+                    </p>
                   </div>
                 </div>
-              </div>
+                <div className='favorite-post'>
+                  <div className='header'>
+                    <div className='tab'>
+                      <p>FAVORITE POST</p>
+                    </div>
+                    <div className='arrow-icon'>
+                      <ArrowForward className='props'/>
+                      <ArrowForward className='props'/>
+                    </div>
+                  </div>
+                  <div className='data'>
+                    <p>Training new Participants</p>
+                    <div className='value'>
+                      <p>38.2</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='statistics'>
+                  <div className='header'>
+                    <div className='tab'>
+                      <p>STATISTICS</p>
+                    </div>
+                    <div className='arrow-icon'>
+                      <ArrowForward className='props'/>
+                      <ArrowForward className='props'/>
+                    </div>
+                  </div>
+                  <div className='data'>
+                    <p>Training new Participants</p>
+                    <div className='graph'>
+                      <div className='vbars'>
+                        <div className='bar val1'></div>
+                        <div className='bar val2'></div>
+                        <div className='bar val3'></div>
+                        <div className='bar val4'></div>
+                        <div className='bar val5'></div>
+                        <div className='bar val6'></div>
+                      </div>
+                      <div className='h-line'></div>
+                    </div>
+                  </div>
+                </div>
               </Container>
             </Container>
             <Container fluid className='footer'>
